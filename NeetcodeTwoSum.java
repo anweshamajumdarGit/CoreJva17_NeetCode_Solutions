49df43932787c0b7fe5dd92f9b8fe4c8f90f59e4
AnweshaMajumder <anwesha.majumdar@gmail.com>
2023-05-17 20:19:12 +0530

    NeetcodeTwoSum

--- NeetcodeTwoSum.java
+++ NeetcodeTwoSum.java
@@ -1,18 +1,40 @@
+import java.util.HashMap;
+
 public class NeetcodeTwoSum {
     public static void main(String[] args) {
         System.out.println("two sum problem");
         TwoSumSolution ts=new TwoSumSolution();
         int[] numbers={3,4,5,6};
-        int length=ts.solution(numbers);
-        int numPos=numbers[2];
-        System.out.println(length);
-        System.out.println(numPos);
+        int targetSum=11;
+        try {
+            ts.solution(numbers,targetSum);
+            throw new IllegalArgumentException("No two numbers add up to the target.");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        
     }
 }
 class TwoSumSolution{
-    public int solution(int[] numbers){
-        
-    return numbers.length;
+    public int[] solution(int[] numbers,int targetSum){
 
+    HashMap<Integer, Integer> prevMap = new HashMap<>();
+    int length=numbers.length;
+    for(int i=0;i<length;i++){
+
+        int num=numbers[i];
+        int diff = targetSum - num;
+        if (prevMap.containsKey(diff)) {
+            System.out.println(prevMap.get(diff));
+            System.out.println(i);
+            return new int[] { prevMap.get(diff), i };
+            
+        }
+        prevMap.put(num, i);
+    }
+    return new int[] {};
     }
 }
+
+
